723eed35860af29bacc3c3b3feae0d8b21c656de
ariffebri22 <muhammadariffebriansyah@gmail.com>
2023-07-08 16:56:49 +0700

    Update Description

--- soal1.js
+++ soal1.js
@@ -1,45 +1,53 @@
 //1. Concat()
+//Untuk menggabungkan kedua array
 const array1 = ["a", "b", "c"];
 const array2 = ["d", "e", "f"];
 
 const combinedArray = array1.concat(array2);
-// console.log(combinedArray);
+console.log(combinedArray);
 
 //2. toString()
+//Untuk mengubah deretan array menjadi string dipisahkan dengan koma
 const arrayString = combinedArray.toString();
-// console.log(arrayString);
+console.log(arrayString);
 
 //3. Find()
+//Untuk menemukan deretan array sesuai dengan kondisi
 let angka = [1, 4, 5, 10, 20, 6, 7, 8, 30, 9, 6, 5, 4];
 let angka2 = angka.find(function (e) {
     return e >= 6;
 });
-// console.log(angka2);
+console.log(angka2);
 
 //4. shift()
+//Untuk menghapus array index ke [0], lalu array sisanya akan menempati array ke [0]
 const hoby = ["Swimming", "Ngoding", "Gaming"];
 const firstHoby = hoby.shift();
-// console.log(hoby);
+console.log(hoby);
 
 //5. unshift()
+//Untuk menambahkan value ke array dari index ke [0]
 const skill = ["html", "css", "js"];
 const skilll = skill.unshift("sass", "webpack");
 
-// console.log(skilll);
+console.log(skilll);
 
 //6. push()
+//Untuk menambahkan value ke dalam array mulai dari index terakhir
 const city = ["paris", "london", "singaparna"];
 const cityy = city.push("california", "manila");
 
-// console.log(city);
+console.log(city);
 
 //7. pop()
+//Untuk menghapus value dari array mulai dari index terakhir
 const country = ["UK", "US", "IND"];
 const countryy = country.pop();
 
-// console.log(country);
+console.log(country);
 
 //8. Object.create()
+//Untuk membuat sebuah object prototipe
 const methodMahasiswa = {
     makan(porsi) {
         this.energi += porsi;
@@ -66,6 +74,7 @@ function Mahasiswa(nama, energi) {
 let arif = Mahasiswa("Arif", 10);
 
 //9. Object.keys()
+//Untuk membuat sebuah array baru berdasarkan value dari sebuah object
 const person = {
     name: "Arif",
     age: 22,
@@ -73,14 +82,15 @@ const person = {
 };
 
 const keys = Object.keys(person);
-// console.log(keys);
+console.log(keys);
 
 //10. Object.values()
+//Untuk membuat sebuah array baru berdasarkan properti dari sebuah object
 const person1 = {
     name: "Arif",
-    age: 22,
+    age: 23,
     city: "Cianjur",
 };
 
 const keys1 = Object.values(person1);
-// console.log(keys1);
+console.log(keys1);
