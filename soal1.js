6898cc617dce0fdc2229f38b4a88bffaa3e534dc
ariffebri22 <muhammadariffebriansyah@gmail.com>
2023-07-09 09:00:33 +0700

    Update function

--- soal1.js
+++ soal1.js
@@ -4,12 +4,12 @@ const array1 = ["a", "b", "c"];
 const array2 = ["d", "e", "f"];
 
 const combinedArray = array1.concat(array2);
-console.log(combinedArray);
+// console.log(combinedArray);
 
 //2. toString()
 //Untuk mengubah deretan array menjadi string dipisahkan dengan koma
 const arrayString = combinedArray.toString();
-console.log(arrayString);
+// console.log(arrayString);
 
 //3. Find()
 //Untuk menemukan deretan array sesuai dengan kondisi
@@ -17,34 +17,34 @@ let angka = [1, 4, 5, 10, 20, 6, 7, 8, 30, 9, 6, 5, 4];
 let angka2 = angka.find(function (e) {
     return e >= 6;
 });
-console.log(angka2);
+// console.log(angka2);
 
 //4. shift()
 //Untuk menghapus array index ke [0], lalu array sisanya akan menempati array ke [0]
 const hoby = ["Swimming", "Ngoding", "Gaming"];
 const firstHoby = hoby.shift();
-console.log(hoby);
+// console.log(hoby);
 
 //5. unshift()
 //Untuk menambahkan value ke array dari index ke [0]
 const skill = ["html", "css", "js"];
 const skilll = skill.unshift("sass", "webpack");
 
-console.log(skilll);
+// console.log(skilll);
 
 //6. push()
 //Untuk menambahkan value ke dalam array mulai dari index terakhir
 const city = ["paris", "london", "singaparna"];
 const cityy = city.push("california", "manila");
 
-console.log(city);
+// console.log(city);
 
 //7. pop()
 //Untuk menghapus value dari array mulai dari index terakhir
 const country = ["UK", "US", "IND"];
 const countryy = country.pop();
 
-console.log(country);
+// console.log(country);
 
 //8. Object.create()
 //Untuk membuat sebuah object prototipe
@@ -82,7 +82,7 @@ const person = {
 };
 
 const keys = Object.keys(person);
-console.log(keys);
+// console.log(keys);
 
 //10. Object.values()
 //Untuk membuat sebuah array baru berdasarkan properti dari sebuah object
@@ -93,4 +93,4 @@ const person1 = {
 };
 
 const keys1 = Object.values(person1);
-console.log(keys1);
+// console.log(keys1);
