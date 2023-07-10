c73243f170176d0c32a39d9a6558a7629f2e0e40
ariffebri22 <muhammadariffebriansyah@gmail.com>
2023-07-10 07:19:28 +0700

    Update Error handling

--- soal3.js
+++ soal3.js
@@ -1,18 +1,18 @@
 function SeleksiNilai(nilaiAwal, nilaiAkhir, dataArray) {
     if (typeof nilaiAwal !== "number" || typeof nilaiAkhir !== "number") {
-        return console.log("Nilai harus berupa number");
+        throw new Error("Nilai harus berupa number");
     }
 
     if (!Array.isArray(dataArray)) {
-        return console.log("Data harus berupa array");
+        throw new Error("Data harus berupa array");
     }
 
     if (nilaiAwal >= nilaiAkhir) {
-        return console.log("Nilai akhir harus lebih besar dari nilai awal");
+        throw new Error("Nilai akhir harus lebih besar dari nilai awal");
     }
 
     if (dataArray.length <= 5) {
-        return console.log("Jumlah angka dalam dataArray harus lebih dari 5");
+        throw new Error("Jumlah angka dalam dataArray harus lebih dari 5");
     }
 
     const results = dataArray.filter((data) => {
@@ -20,7 +20,7 @@ function SeleksiNilai(nilaiAwal, nilaiAkhir, dataArray) {
     });
 
     if (results.length === 0) {
-        return console.log("Nilai tidak ditemukan");
+        throw new Error("Nilai tidak ditemukan");
     }
 
     results.sort((a, b) => {
@@ -30,9 +30,13 @@ function SeleksiNilai(nilaiAwal, nilaiAkhir, dataArray) {
     console.log(results);
 }
 
-SeleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8]);
-SeleksiNilai(15, 3, [2, 25, 4, 14, 17, 30, 8]);
-SeleksiNilai(5, 17, [2, 25, 4]);
-SeleksiNilai(5, 17, [2, 25, 4, 1, 30, 18]);
-SeleksiNilai("a", 10, [2, 25, 4, 14, 17, 30, 8]);
-SeleksiNilai(5, 10, "not an array");
+try {
+    SeleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8]);
+    SeleksiNilai(15, 3, [2, 25, 4, 14, 17, 30, 8]);
+    SeleksiNilai(5, 17, [2, 25, 4]);
+    SeleksiNilai(5, 17, [2, 25, 4, 1, 30, 18]);
+    SeleksiNilai("a", 10, [2, 25, 4, 14, 17, 30, 8]);
+    SeleksiNilai(5, 10, "1, 2, 3, 4, 5");
+} catch (err) {
+    console.log(err.message);
+}
